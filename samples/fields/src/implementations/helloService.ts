7d12041a8725c9f659c75065c0457ad4b075908e
Ron Buckton <rbuckton@chronicles.org>
2021-03-14 17:58:14 -0700

    Update fields sample

--- samples/fields/src/implementations/helloService.ts
+++ samples/fields/src/implementations/helloService.ts
@@ -2,9 +2,7 @@ import { IHelloService } from "../services/iHelloService";
 import { ITranslateService } from "../services/iTranslateService";
 
 export class HelloService implements IHelloService {
-    constructor(
-        @ITranslateService private translation: ITranslateService
-    ) {}
+    @ITranslateService private translation: ITranslateService
 
     sayHello(name: string): string {
         return this.translation.format("Hello, {name}!", { name });
